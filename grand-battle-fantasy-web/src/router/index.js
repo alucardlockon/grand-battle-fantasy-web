e8034d8ff6eb98afc9b86486ba9e4043246f785d
yuwh <alucardlockon3@gmail.com>
2018-05-31 01:42:33 +0800

    init vue

--- grand-battle-fantasy-web/src/router/index.js
+++ grand-battle-fantasy-web/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Register from '@/components/account/register'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.use(Router)
+Vue.use(ElementUI)
+Vue.use(VueAxios,axios)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Register',
+      component: Register
     }
   ]
 })
